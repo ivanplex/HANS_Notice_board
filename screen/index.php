866100bc06ecb8424a7ff2215801c1410f41bc8b
ivan <ivan@ivanplex.com>
2018-01-03 04:04:33 +0000

    Added tester

---
+++ screen/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<script type="text/javascript" src="update.js"></script>
+		<link rel="stylesheet" type="text/css" href="single-image.css">
+	</head>
+
+	<body>
+		<div class="single-image"></div>
+	</body>
+</html>
